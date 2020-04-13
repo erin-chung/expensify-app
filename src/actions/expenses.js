5424c9ef78fb20f18dab030082475ab5b00c3634
Erin Chung <erinchung@Erins-MacBook-Pro.local>
2020-04-13 03:58:01 -0600

    added google login auth

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,3 @@
-import uuid from 'react-uuid'
 import database from '../db/firebase'
 
 //ADD_EXPENSE action generator
@@ -8,7 +7,8 @@ export const addExpense = (expense) => ({
 })
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch)=>{
+    return (dispatch, getState)=>{
+        const uid = getState().auth.uid
         const {
             description = '', 
             note = '', 
@@ -17,7 +17,7 @@ export const startAddExpense = (expenseData = {}) => {
         } = expenseData //destructuring for code below
         const expense = { description, note, amount, createdAt }
         //Save to database and push to redux store
-        database.ref('expenses').push(expense).then((ref)=>{
+        database.ref(`users/${uid}/expenses`).push(expense).then((ref)=>{
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -32,9 +32,53 @@ export const removeExpense = ({ id } = {}) => ({
     id
 })
 
+export const startRemoveExpense = (({id} = {})=>{
+    return (dispatch,getState)=>{
+        const uid = getState().auth.uid
+        database.ref(`users/${uid}/expenses/${id}`).remove().then(()=>{
+            dispatch(removeExpense({ id }))
+        })
+    }
+})
+
+
 //EDIT_EXPENSE action generator
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 })
+
+export const startEditExpense = (id,updates) => {
+    return (dispatch,getState)=>{
+        const uid = getState().auth.uid
+            return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(()=>{
+                dispatch(editExpense(id,updates))
+        })
+    }
+}
+
+//SET_EXPENSES action generator
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+export const startSetExpenses = () => {
+    //sets database data to redux store
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid
+        
+        return database.ref(`users/${uid}/expenses`).once('value').then((snapshot)=>{
+            const expenses = []
+            snapshot.forEach((childSnapshot)=>{
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                })
+            })
+            dispatch(setExpenses(expenses))
+        })
+        
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import AppRouter from './routers/AppRouter'
+import AppRouter, {history} from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import {startSetExpenses} from './actions/expenses'
+import {login, logout} from './actions/auth'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-
-import './db/firebase'
+import { firebase } from './db/firebase'
 
 const store = configureStore()
 
@@ -15,5 +16,31 @@ const jsx = (
         <AppRouter />
     </Provider>
 )
+let hasRendered = false
+const renderApp = () => {
+    if(!hasRendered){
+        ReactDOM.render(jsx, document.getElementById('app'))
+        hasRendered = true
+    }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        store.dispatch(login(user.uid))
+        console.log('uid', user.uid)
+        store.dispatch(startSetExpenses()).then(()=>{
+           renderApp()
+           if(history.location.pathname === '/'){
+               history.push('/dashboard')
+           }
+        })
+    } else {
+        store.dispatch(logout())
+        console.log('logout')
+        renderApp()
+        history.push('/')
+    }
+})
